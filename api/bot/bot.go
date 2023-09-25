aba776b054cad9fbecd363772099e78203e4fedb
Stanislau Matskevich <st.matskevich@gmail.com>
2023-09-25 20:18:31 +0200

    Implemented ticket invoice handling

--- api/bot/bot.go
+++ api/bot/bot.go
@@ -4,4 +4,7 @@ import "github.com/PaulSonOfLars/gotgbot/v2"
 
 type BotInteractor interface {
 	SendMessage(chatId int64, text string, opts *gotgbot.SendMessageOpts) (*gotgbot.Message, error)
+	AnswerCallbackQuery(callbackQueryId string, opts *gotgbot.AnswerCallbackQueryOpts) (bool, error)
+	SendInvoice(chatId int64, title string, description string, payload string, providerToken string, currency string, prices []gotgbot.LabeledPrice, opts *gotgbot.SendInvoiceOpts) (*gotgbot.Message, error)
+	AnswerPreCheckoutQuery(preCheckoutQueryId string, ok bool, opts *gotgbot.AnswerPreCheckoutQueryOpts) (bool, error)
 }
