0f74e60ac95dce0ff529b42d6fc752fc988f06fa
Stanislau Matskevich <st.matskevich@gmail.com>
2023-10-01 16:37:12 +0200

    Updated top-level documentation

--- ui/setup-env-config.sh
+++ ui/setup-env-config.sh
@@ -5,6 +5,6 @@ output_file="$APP_DIR/env-config.js"
 for var in $(env | grep '^REACT_APP_'); do
   key=$(echo "$var" | cut -d '=' -f 1)
   value=$(echo "$var" | cut -d '=' -f 2-)
-  value=$(echo "$value" | sed 's/"/\\"/g')  # Escape double quotes
+  value=$(echo "$value" | sed 's/"/\\"/g')
   echo "window.REACT_APP_ENV.$key = \"$value\";" >> "$output_file"
 done
